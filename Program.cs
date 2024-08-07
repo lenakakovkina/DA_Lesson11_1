c79f9e7e4296f0b3ddebffd296a04bcf92d71f8d
Olena <elenakakovkina89@gmail.com>
2024-08-07 13:35:23 +0200

    Wright ToDo list in file

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿
+﻿using System.Threading.Tasks;
+
 List<string> notDoneTask = new List<string>();
 List<string> doneTask = new List<string>();
-string input="";
-
-while (input !="stop")
+string input = "";
+var filename = "C:/TESTING/testOlena.txt";
+while (input != "stop")
 {
 
     Console.WriteLine("Hi it is To do list. Input your task, please");
@@ -24,6 +25,7 @@ while (input !="stop")
     foreach (string task in notDoneTask)
     {
         Console.WriteLine(task);
+        
     }
 
     Console.WriteLine("\nIt is done list:");
@@ -32,4 +34,22 @@ while (input !="stop")
         Console.WriteLine(task);
     }
 
+}
+// Writing the active to-do list to the file
+using (var streamWriter = new StreamWriter(filename)) { }//to wipe file
+    using (var streamWriter = new StreamWriter(filename, true))
+{
+    streamWriter.WriteLine("Active To-Do List:");
+    foreach (string task in notDoneTask)
+    {
+        streamWriter.WriteLine(task);
+    }
+}
+using (var streamWriter = new StreamWriter(filename, true))
+{
+    streamWriter.WriteLine("Done To-Do List:");
+    foreach (string task in doneTask)
+    {
+        streamWriter.WriteLine(task);
+    }
 }
