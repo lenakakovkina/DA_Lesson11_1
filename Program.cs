c28513c87fcf8cc8fa5f98e64c38ae156e21e757
Olena <elenakakovkina89@gmail.com>
2024-08-07 22:29:54 +0200

    Add possibility to read from file if it exist

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
 ﻿using System.Threading.Tasks;
-
 List<string> notDoneTask = new List<string>();
 List<string> doneTask = new List<string>();
 string input = "";
 var filename = "C:/TESTING/testOlena.txt";
+if (File.Exists(filename))
+{
+    using (var streamReader = new StreamReader(filename))
+    {
+        string line;
+        bool isDoneList = false;
+        while ((line = streamReader.ReadLine()) != null)
+        {
+            if (line == "Active To-Do List:")
+            {
+                isDoneList = false;
+            }
+            else if (line == "Done To-Do List:")
+            {
+                isDoneList = true;
+            }
+            else
+            {
+                if (isDoneList)
+                {
+                    doneTask.Add(line);
+                }
+                else
+                {
+                    notDoneTask.Add(line);
+                }
+            }
+        }
+    }
+}
+
+
 while (input != "stop")
 {
 
@@ -25,7 +56,7 @@ while (input != "stop")
     foreach (string task in notDoneTask)
     {
         Console.WriteLine(task);
-        
+
     }
 
     Console.WriteLine("\nIt is done list:");
@@ -35,9 +66,10 @@ while (input != "stop")
     }
 
 }
-// Writing the active to-do list to the file
-using (var streamWriter = new StreamWriter(filename)) { }//to wipe file
-    using (var streamWriter = new StreamWriter(filename, true))
+//Writing to-do list to the file
+using (var streamWriter = new StreamWriter(filename))
+{ }
+using (var streamWriter = new StreamWriter(filename, true))
 {
     streamWriter.WriteLine("Active To-Do List:");
     foreach (string task in notDoneTask)
